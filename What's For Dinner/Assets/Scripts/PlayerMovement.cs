6fd4419208d902608863b0a9a87ba806fbbdef4a
cdm456852 <576216884@qq.com>
2025-03-28 13:14:58 -0400

    Update PlayerMovement.cs

--- What's For Dinner/Assets/Scripts/PlayerMovement.cs
+++ What's For Dinner/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+
+    [SerializeField] private Rigidbody2D _Rigidbody2D;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 direction = new Vector2(horizontal, vertical).normalized;
+        _Rigidbody2D.linearVelocity = direction * speed;
     }
 }
