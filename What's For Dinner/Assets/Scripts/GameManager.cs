e7d76ae87c15ac8a950ff7731143b7c84463fb8a
cdm456852 <576216884@qq.com>
2025-03-28 14:58:06 -0400

    added the timer

---
+++ What's For Dinner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float maximamTime;
+    public TextMeshProUGUI txtTimer;
+
+    private float timer;
+
+    private void Awake()
+    {
+        timer = maximamTime;
+    }
+
+    private void Update()
+    {
+        UpdateTimer();
+    }
+
+    private void UpdateTimer()
+    {
+        timer -= Time.deltaTime;
+
+        int hours = (int)(timer / 3600);
+        int minutes = (int)((timer - hours * 3600) / 60);
+        int seconds = (int)(timer - hours * 3600 - minutes * 60);
+        int milliSeconds = (int)((timer - ((int)(timer))) * 1000);
+
+        string outputTxt = $"{hours}:{minutes}:{seconds}:{milliSeconds}";
+        txtTimer.text = outputTxt;
+    }
+}
