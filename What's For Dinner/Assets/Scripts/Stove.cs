53598cead4b6e66c989869023def685d12743b20
cdm456852 <576216884@qq.com>
2025-03-28 15:28:30 -0400

    Update Stove.cs

--- What's For Dinner/Assets/Scripts/Stove.cs
+++ What's For Dinner/Assets/Scripts/Stove.cs
@@ -1,13 +1,42 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Stove : Interactable
 {
+    public Button junkFood, healthyFood;
     public GameObject clock;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        junkFood.onClick.RemoveAllListeners();
+        healthyFood.onClick.RemoveAllListeners();
+
+        junkFood.onClick.AddListener(OnJunkFoodClick);
+        healthyFood.onClick.AddListener(OnHealthyFoodClick);
+
+        EnableButtons(false);
+    }
+
+    private void OnHealthyFoodClick()
+    {
+        PlayerPrefs.SetFloat("HighScore", 1000);
+
+        EnableButtons(false);
+    }
+
+    private void OnJunkFoodClick()
+    {
+        PlayerPrefs.SetFloat("HighScore", -1000);
+
+        EnableButtons(false);
+    }
+
+    private void EnableButtons(bool buttonState)
+    {
+        junkFood.gameObject.SetActive(buttonState);
+        healthyFood .gameObject.SetActive(buttonState);
     }
 
     // Update is called once per frame
@@ -23,5 +52,7 @@ public class Stove : Interactable
         Tic clockTic = clock.GetComponent<Tic>();
 
         clockTic.maxTime = 10f;
+
+        EnableButtons(true);
     }
 }
